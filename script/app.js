9313ac2e929eed2c52c7fcd8fdb895a20a62c423
Shushanik Simonyan <43413082+lilysimon351@users.noreply.github.com>
2021-12-13 13:20:36 +0400

    fixed header; checkbox change color on hover; chnage popup btn color

--- script/app.js
+++ script/app.js
@@ -22,3 +22,19 @@ popup?.addEventListener('click', (event) => {
 popupInner?.addEventListener('click', (event) => {
 	event.stopPropagation();
 })
+
+// fix header
+lastScrollTop = 60;
+document.addEventListener("DOMContentLoaded", fixHeader);
+window.addEventListener("scroll", fixHeader);
+function fixHeader() {
+	var st = window.pageYOffset || document.documentElement.scrollTop,
+	header = document.querySelector('.header'),
+	className = 'header__blue';
+	if (st > lastScrollTop){
+	   header.classList.add(className)
+	} else {
+	   header.classList.remove(className)
+
+	}
+}
